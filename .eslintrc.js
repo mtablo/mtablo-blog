af2e499263a9d280beabf1237cd8172141140f7b
mtablo <mtablo@lunit.io>
2022-07-11 16:50:08 +0900

    feat: initial typescript/eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,91 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["import"],
+  rules: {
+    "prefer-destructuring": [
+      "error",
+      {
+        array: true,
+        object: true,
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+    "no-console": [
+      "error",
+      {
+        allow: ["error"],
+      },
+    ],
+    "@typescript-eslint/no-empty-function": [
+      "error",
+      {
+        allow: ["arrowFunctions"],
+      },
+    ],
+    "no-duplicate-imports": [
+      "error",
+      {
+        includeExports: true,
+      },
+    ],
+    // "no-restricted-imports": [
+    //   "error",
+    //   {
+    //     paths: [
+    //       "@mui/icons-material",
+    //       "@mui/material",
+    //       "@mui/lab",
+    //       "date-fns",
+    //       "react-dom/test-utils",
+    //     ],
+    //     patterns: ["../*", "@mui/*/*/*", "!@mui/material/test-utils/*"],
+    //   },
+    // ],
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@mui/**",
+            group: "external",
+            position: "after",
+          },
+          {
+            pattern: "components/**",
+            group: "internal",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+    "no-multiple-empty-lines": ["error", { max: 1 }],
+    "@typescript-eslint/explicit-module-boundary-types": ["off"],
+    "no-return-await": "error",
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": ["error"],
+      },
+    },
+  ],
+  ignorePatterns: ["src/**/public/**/*", "cypress/**"],
+};
